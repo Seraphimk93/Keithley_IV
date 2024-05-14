903f8b501b0bf6be9226a25362a3bf53a28e55e1
Seraphim Koulosousas <koulosousas@pplxdaq36.nat.physics.ox.ac.uk>
2024-05-14 20:20:28 +0100

    Fixed loads of bugs

--- keithley_IV.py
+++ keithley_IV.py
@@ -17,32 +17,37 @@ import numpy as np
 from keithleyComms  import Keithley2450
 from IV_Analysis import IVAnalysis
 import csv
+import random
 
 
-class TakeIV(QThread,Keithley2450):
+class TakeIV(QThread):
     dataChanged = pyqtSignal(list)
     def __init__(self,start,end,step,timeDelay,Resource, *args,**kwargs):
         QThread.__init__(self, *args, **kwargs)
-        Keithley2450.__init__(self,Resource)
+        # Keithley2450.__init__(self,Resource)
         self.StartV = start
         self.EndV = end
         self.Steps = step
         self.timeDelay = timeDelay
+        self.k = Resource
         
         #self.Enable()
         print(self.k)
         print("connected")
+        # print("test in class",self.k.test())
         
     def TakeIV(self):
         self.STOP = False
         steps = (float(self.EndV)-float(self.StartV))/float(self.Steps)
-        print("steps:", steps)
+        # print("steps:", steps)
         for i in range(int(steps)):
             if self.STOP ==True:
                 break
-            self.SourceVoltage(float(self.StartV) + i*float(self.Steps))
-            V = self.MeasureVoltage()
-            I = self.MeasureCurrent()
+            # print("steps:",float(self.StartV) + i*float(self.Steps))
+            v = float(self.StartV) + i*float(self.Steps)
+            self.k.SourceVoltage(v)
+            V = self.k.MeasureVoltage()
+            I = self.k.MeasureCurrent()
             #I = np.random.uniform(0,1)
             #V = float(self.StartV) + i*float(self.Steps)
             print(V,I)
@@ -56,53 +61,56 @@ class TakeIV(QThread,Keithley2450):
         self.finished.emit()
 
 
-class RampDown(QThread,Keithley2450):
+class RampDown(QThread):
     dataChanged = pyqtSignal(list)
     def __init__(self,start,end,step,timeDelay,Resource, *args,**kwargs):
         QThread.__init__(self, *args, **kwargs)
-        Keithley2450.__init__(self,Resource)
         self.StartV = start
         self.EndV = end
         self.Steps = step
         self.timeDelay = timeDelay
+        self.k  = Resource
 
-        self.Enable()
+        self.k.Enable()
         print("connected")
         
     def RampDown(self):
         self.STOP = False
+        self.EndV = self.k.MeasureVoltage()
         steps = (float(self.EndV)-float(self.StartV))/float(self.Steps)
-        print("steps:", steps)
+        # print("steps:", steps)
         for i in range(int(steps)):
             if self.STOP ==True:
                 break
-            self.SourceVoltage(float(self.EndV) - i*float(self.Steps))
+            self.k.SourceVoltage(float(self.EndV) - i*float(self.Steps))
             #V = self.MeasureVoltage()
-            I = self.MeasureCurrent()
+            I = self.k.MeasureCurrent()
             #I = np.random.uniform(0,1)
             V = float(self.EndV) - i*float(self.Steps)
             self.dataChanged.emit([float(V),float(I)])
             # self.dataChanged.emit('{}'.format(np.random.uniform(0.5,0.7)))
             QThread.msleep(int(self.timeDelay))
-
+        self.k.SourceVoltage(0)
+        I = self.k.MeasureCurrent()
+        self.dataChanged.emit([float(0),float(I)])
 
     def end(self):
         self.STOP = True
         self.finished.emit()
 
 
-class Track(QThread,Keithley2450):
+class Track(QThread):
     dataChanged = pyqtSignal(list)
     def __init__(self,timeDelay,Resource, *args,**kwargs):
         QThread.__init__(self, *args, **kwargs)
-        Keithley2450.__init__(self,Resource)
         self.timeDelay= timeDelay
+        self.k=Resource
         
     def TrackIV(self):
         self.STOP = False
         while self.STOP==False:
-            V = self.MeasureVoltage
-            I = self.MeasureCurrent
+            V = self.k.MeasureVoltage
+            I = self.k.MeasureCurrent
             self.dataChanged.emit([V,I])
             # self.dataChanged.emit('{}'.format(np.random.uniform(0.5,0.7)))
             QThread.msleep(int(self.timeDelay))
@@ -114,11 +122,11 @@ class Track(QThread,Keithley2450):
 
 
 
-class MainWindow(QMainWindow,Keithley2450):
+class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         # super(MainWindow, self).__init__(*args, **kwargs)
         QMainWindow.__init__(self,*args,**kwargs)
-        Keithley2450.__init__(self)
+        self.k = Keithley2450()
 
         pg.setConfigOptions(antialias=True)
         self.setWindowTitle("Keithley IV")
@@ -127,10 +135,11 @@ class MainWindow(QMainWindow,Keithley2450):
         self.Volts = []
         self.Current = []
         self.clim = 8e-05 # current limit [Amps]
-        self.Kdict = {"Keithley 1": "Address 1","Keithley 2":"Address 2" }
+        self.Kdict = {"Keithley 1": 'USB0::0x05E6::0x2450::04490665::INSTR',"Keithley 2":'USB0::0x05E6::0x2450::04563534::INSTR',"Keithley 3":'USB0::1510::9296::04618475::0::INSTR' }
         self.Kcombobox = QComboBox()
         self.Kcombobox.addItem('Keithley 1')
         self.Kcombobox.addItem('Keithley 2')
+        self.Kcombobox.addItem('Keithley 3')
         # Kcombobox.addItem('Three')
         # Kcombobox.addItem('Four')
         
@@ -155,13 +164,16 @@ class MainWindow(QMainWindow,Keithley2450):
         self.graphWidget.getAxis("bottom").label.setFont(my_font)
         self.graphWidget.getAxis("left").label.setFont(my_font)
         self.graphWidget.setBackground('#000000')
-        pen = pg.mkPen(color=(105, 255, 255),width=5)
+        # pen = pg.mkPen(color=(105, 255, 255),width=5)
         self.x1 = [1,2,3,4]
         self.y1 = [0.5,3,2,3]
         self.x2 = [2,3,4,5]
         self.y2 = [0,1,2,3]
         self.graphWidget.addLegend()
         self.graphWidget.showGrid(x=True, y=True)
+        r,g,b = random.randint(50,200),random.randint(50,200),random.randint(50,200) #Randomise IV Colours
+        pen = pg.mkPen(color=(r, g, b),width=2.5)
+        # self.graphWidget.plot(self.Volts, self.Current, pen=pen,fillLevel=-0.3,name=f"IV Curve")
         self.data_line =  self.graphWidget.plot(self.Volts, self.Current, pen=pen,fillLevel=-0.3, brush=(105,105,105,50),name="IV Curve")
         # self.graphWidget.plot(self.x1, self.y1, pen=pen,fillLevel=-0.3, brush=(105,105,105,50),name = "Sensor 1")
         # self.graphWidget.plot(self.x2, self.y2, pen=pg.mkPen(color=(105, 255, 255),width=5),fillLevel=-0.3, brush=(105,105,105,50),name = "Sensor 2")
@@ -212,10 +224,10 @@ class MainWindow(QMainWindow,Keithley2450):
         #connect keithley 1 controls to functions
         self.keithley_connect.clicked.connect(self.ConnectKeithley)
         self.keithley_Disconnect.clicked.connect(self.Disconnect)
-        self.keithley_Enable.clicked.connect(self.Enable)
-        self.keithley_Disable.clicked.connect(self.Disable)
+        self.keithley_Enable.clicked.connect(self.k.Enable)
+        self.keithley_Disable.clicked.connect(self.k.Disable)
         # self.keithley_SetCurrentLimit.clicked.connect(self.SetCurrentLimit)
-        self.keithley_takeIV.clicked.connect(self.TakeIV)
+        self.keithley_takeIV.clicked.connect(self.TakeIVfunction)
         self.StopButton.clicked.connect(self.Stop)
         self.ClearCanvasButton.clicked.connect(self.ClearCanvas)
         self.Open.clicked.connect(self.OpenPlot)
@@ -235,7 +247,7 @@ class MainWindow(QMainWindow,Keithley2450):
         self.k1start = QLineEdit("0")
         self.k1end = QLineEdit("75")
         self.k1steps = QLineEdit("0.2")
-        self.k1timeDelay =QLineEdit("0.0001")
+        self.k1timeDelay =QLineEdit("1")
         self.GoToValueInput =QLineEdit()
         
 
@@ -303,13 +315,14 @@ class MainWindow(QMainWindow,Keithley2450):
     def ConnectKeithley(self):
         address = self.Kdict[self.Kcombobox.currentText()]
         print(address)
-        self.k1 = self.Connect('USB0::0x05E6::0x2450::04490665::INSTR')
+        self.k.Connect(address)
         self.ToggleInputs(True)
         self.Comms.setText("Keithley is Connected")
         self.SetCurrentLimit(self.clim)
+        print(self.k.test())
 
     def SetCurrentLimit(self,value):
-        self.Source_Current_Limit(value)
+        self.k.Source_Current_Limit(value)
         self.Comms.setText("Current limit set to: {} Amps".format(value))
         self.clim = "{}".format(value)
 
@@ -321,16 +334,17 @@ class MainWindow(QMainWindow,Keithley2450):
         self.ToggleInputs(False)
         
 
-    def TakeIV(self):
+    def TakeIVfunction(self):
         if len(self.k1current.text())==0:
             self.Comms.setText("Please Insert Current limit")
             return
+        # print("take iv function: ", self.k.test())
         start = self.k1start.text()
         end = self.k1end.text()
         step = self.k1steps.text()
         timeDelay = self.k1timeDelay.text()
         self.thr = QThread()
-        self.worker = TakeIV(start,end,step,timeDelay,self.k1)
+        self.worker = TakeIV(start,end,step,timeDelay,self.k)
         self.worker.moveToThread(self.thr)
         self.thr.started.connect(self.worker.TakeIV)
         self.worker.dataChanged.connect(self.update_plot)
@@ -344,6 +358,7 @@ class MainWindow(QMainWindow,Keithley2450):
         end = self.k1end.text()
         step = self.k1steps.text()
         timeDelay = self.k1timeDelay.text()
+        self.Stop()
         self.RDthr = QThread()
         self.RDworker = RampDown(start,end,step,timeDelay,self.k)
         self.RDworker.moveToThread(self.RDthr)
@@ -357,13 +372,25 @@ class MainWindow(QMainWindow,Keithley2450):
     def Stop(self):
         self.stop = True
         self.worker.end()
-        self.RDworker.end()
-        self.Trackworker.end()
-
+        try:
+            if self.RDthr.isRunning():
+                self.RDworker.end()
+        except RuntimeError:
+            print()
+        except AttributeError:
+            print()
+        try:
+            if self.thr.isRunning():
+                self.thr.end()
+        except RuntimeError:
+            print()
+        except AttributeError:
+            print()
     def ClearCanvas(self):
-        self.Volts=[]
-        self.Current=[]
-        self.data_line.setData(self.Volts, self.Current)  # Update the data.
+        self.Volts = []
+        self.Current= []
+        self.data_line.clear()
+        # self.data_line.setData(self.Volts, self.Current)  # Update the data.
         self.LiveVoltage.setText(str("%.2f" % round(float(0), 2)))
         self.LiveCurrent.setText(str("%.2f" % round(float(0), 2)))
         
@@ -372,11 +399,11 @@ class MainWindow(QMainWindow,Keithley2450):
         if len(self.k1current.text())==0:
             self.Comms.setText("Please Insert Current limit")
             return
-        self.SourceVoltage(self.GoToValueInput.text())
-        I=self.MeasureCurrent()
+        self.k.SourceVoltage(self.GoToValueInput.text())
+        I=self.k.MeasureCurrent()
         self.Volts.append(float(self.GoToValueInput.text()))
         self.Current.append(float(I))
-        self.data_line.setData(self.Volts, self.Current)
+        self.data_line.plot(self.Volts, self.Current)
         self.LiveVoltage.setText(str("Voltage [V]: %.2f" % round(float(self.GoToValueInput.text()), 2)))
         self.LiveCurrent.setText(str("Current [A]: %.2f" % round(I, 2)))
         
@@ -410,10 +437,21 @@ class MainWindow(QMainWindow,Keithley2450):
     def update_plot(self,data):
         self.Volts.append(float(data[0]))
         self.Current.append(float(data[1]))  # Add a new random value.
-        self.data_line.setData(self.Volts, self.Current)  # Update the data.
+        print("update plot:", self.Volts[-1], self.Current[-1] )
+        # r,g,b = random.randint(50,200),random.randint(50,200),random.randint(50,200) #Randomise IV Colours
+        # pen = pg.mkPen(color=(r, g, b),width=2.5)
+        # self.graphWidget.plot(self.Volts, self.Current, pen=pen,fillLevel=-0.3,name=f"IV Curve")
+        self.data_line.setData(np.array(self.Volts), np.array(self.Current))  # Update the data.
+        
+        # QApplication.processEvents()
+        # self.graphWidget.plot(self.Volts, self.Current, pen=pen,fillLevel=-0.3, brush=(105,105,105,50),name="IV Curve")
         self.LiveVoltage.setText(str("%.2f" % round(float(data[0]), 2)))
         self.LiveCurrent.setText(str("%.2f" % round(float(data[1])*1e06, 2)))
-        self.graphWidget.setLimits(xMin=(min(self.Volts)), xMax=max(self.Volts), yMin=min(self.Current), yMax=max(self.Current))
+        self.graphWidget.setXRange(min(self.Volts), max(self.Volts), padding=0)
+        self.graphWidget.setYRange(min(self.Current), max(self.Current))
+        # self.graphWidget.GraphicsScene.mouseEvents.MouseClickEvent()
+        # QApplication.processEvents()
+        # self.graphWidget.setLimits(xMin=(min(self.Volts)), xMax=max(self.Volts), yMin=min(self.Current), yMax=max(self.Current))
         # QApplication.processEvents()
 
     def SavePlot(self):
